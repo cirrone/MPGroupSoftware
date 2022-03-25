03d9bdcace4599fa045b2dcaaec5933c1e1d86be
Beatrice <beatrice.cagni93@gmail.com>
2022-03-25 10:33:48 +0100

    provo a committare

--- EPOCH/conversionPicSdfFile2csv/sdf2csv_unstable/sdf2csv_test.m
+++ EPOCH/conversionPicSdfFile2csv/sdf2csv_unstable/sdf2csv_test.m
@@ -1,4 +1,5 @@
-% GAP Cirrone, November 2018
+% GAP Cirrone, November 2018 - modified by Beatrice Cagni from March 2022
+%
 % This file read a .sdf file produced with PIC 3D simulation by 
 % Dr Martina Zakova (ELI-Beamlines) and convert it in a csv file that can
 % be read by the ELIMED Geant4 application
@@ -7,52 +8,67 @@
 % ELIMED Geant application is Z. X and Z must be hence exchanged both in
 % the momentum and in the position definition
 %
+
+% Step 1: load the single sdf file.
 fileToConvert = GetDataSDF('p0211.sdf')
 
-c=299792458;
-m_e=9.10938e-31;
-e=1.60217662e-19;
-eps0=8.85419e-12;
-hmotnost_proton=1836.2;
-hmotnost_carbon=22032;
+%% 
 
-% Momentum components as derived from the EPOCH code: they are not the
-% cosine directions
-%
+% Set contants.
+c=299792458; % speed of light
+m_e=9.10938e-31; % electron mass in kg
+e=1.60217662e-19; % electron charge in C
+eps0=8.85419e-12; % permittivity in vacuum in F/m
+hmotnost_proton=1836.2; % proton mass in unit of electron masses
+hmotnost_carbon=22032; % carbon mass in unit of electron masses
+
+%% 
+
+% Extraction of the variables of interest: Momentum components as derived from the EPOCH code: they are not the
+% cosine directions.
+
 px_p = fileToConvert.Particles.Px.subset_ionbackground.proton.data; 
 py_p = fileToConvert.Particles.Py.subset_ionbackground.proton.data;
 pz_p = fileToConvert.Particles.Pz.subset_ionbackground.proton.data;
 
+%% 
+
 % pz and px momenta are exchanged to agree with Geant4 ELIMED application
-%
+
 pzFinal = px_p;
 %pxFinal = pz_p;
 pxFinal = py_p; % ONLY FOR 2D PIC
 pyFinal = py_p;
 
+%%
 
-% Particle positions as derived from the EPOCH code
-%
+% Extraction of the variables of interest: Particle positions as derived from the EPOCH code
+
 xx_p = fileToConvert.Particles.Px.subset_ionbackground.proton.grid.x; 
 yy_p = fileToConvert.Particles.Py.subset_ionbackground.proton.grid.y;
 
-% z and x coordinate are exchanged
-%
+%%
+
+% z and x coordinate are exchanged to agree with Geant4 ELIMED application
+
 zFinal = xx_p;
 yFinal = yy_p;
 xFinal = yy_p; %******** only for 2D PIC
 
+%%
 
-% Now I select particles that have only a pz positive value
-%
-index = find(pzFinal > 0);
-pzFinalSelected = pzFinal(index);
-pxFinalSelected = pxFinal(index);
-pyFinalSelected = pyFinal(index);
+% Now I select particles that have only a pz positive value, so those that
+% move forward
 
-zFinalSelected = zFinal(index);
-yFinalSelected = yFinal(index);
+index = find(pzFinal > 0); % find the index
+pzFinalSelected = pzFinal(index); %select the corresponding pz value
+pxFinalSelected = pxFinal(index); %select the corresponding px value
+pyFinalSelected = pyFinal(index); %select the corresponding py value
 
+zFinalSelected = zFinal(index); %select the corresponding z position value
+yFinalSelected = yFinal(index); %select the corresponding y position value
+
+%%
 % pzFinalSelected, py... and px... ARE NOT cosine directions
 % Cosine direction must be calculated from these components
 %
