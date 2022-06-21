75e55509f117d15fafd2107f39aedeca1c9421e6
BeatriceCagni <98459651+BeatriceCagni@users.noreply.github.com>
2022-06-21 11:35:12 +0200

    correzione distribuzione angolare

--- EPOCH/python_codes/functions/plot_angular_distribution.py
+++ EPOCH/python_codes/functions/plot_angular_distribution.py
@@ -15,6 +15,8 @@ def plot_angular_distribution(counts, energy, angle, savefile, time, particle):
     cbar.set_label('dN/dE')
     fig.tight_layout()
     ax1.legend()
+    ax1.set_xticks([-np.pi/2, -np.pi/4, 0, np.pi/4, np.pi/2])
+    ax1.set_xticklabels(['-$\pi$/2', '-$\pi$/4', '0', '$\pi$/4', '$\pi$/2'])
     plt.title(particle + ' ' + time + ' ps')
     plt.savefig(savefile, dpi=200)
     plt.close()
